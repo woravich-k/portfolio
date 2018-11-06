21413484ef0e9de7342871e610550668edc32a52
woravich-k <35569166+woravich-k@users.noreply.github.com>
2018-11-06 14:44:21 +0000

    content + logo (no contact)

---
+++ js/app_contact.js
@@ -0,0 +1,8 @@
+function send(){
+	var name = document.getElementById("Name");
+	var email = document.getElementById("Email");
+	var note = document.getElementById("note");
+	alert(name.value);
+	alert(email.value);
+	alert(note.value);
+}
